3357dd88fdd75b95a5c3c73dd0d40796767472ee
Aneesh Radhakrishnan <aneeshradhakrishnan@aneeshs-mbp.attlocal.net>
2019-09-22 21:38:38 -0400

    Retrofit Rx dependency added

--- app/src/main/java/com/retailer/poc/dealbrowser/app/DealBrowserDaggerModule.kt
+++ app/src/main/java/com/retailer/poc/dealbrowser/app/DealBrowserDaggerModule.kt
@@ -1,6 +1,29 @@
 package com.retailer.poc.dealbrowser.app
 
+import com.google.gson.GsonBuilder
+import com.retailer.poc.dealbrowser.service.DealsService
 import dagger.Module
+import dagger.Provides
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
 
 @Module
-class DealBrowserDaggerModule
+class DealBrowserDaggerModule {
+
+    @Provides
+    fun provideDealService():DealsService {
+        val gson = GsonBuilder()
+                .setLenient()
+                .create()
+
+        val retrofit = Retrofit.Builder()
+                .baseUrl("http://target-deals.herokuapp.com/")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build()
+
+        return retrofit.create(DealsService::class.java)
+    }
+}
---
+++ app/src/main/java/com/retailer/poc/dealbrowser/service/DealsService.kt
@@ -0,0 +1,2 @@
+package com.retailer.poc.dealbrowser.service
+
